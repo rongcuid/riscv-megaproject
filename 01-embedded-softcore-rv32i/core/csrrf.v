dee708ccbcab2c8a44710dcd21c9399bb7c4a282
Rongcui Dong <rongcuid@outlook.com>
2017-12-17 12:46:05 -0800

    Writing CSR decoder

--- 01-embedded-softcore-rv32i/core/csrrf.v
+++ 01-embedded-softcore-rv32i/core/csrrf.v
@@ -6,11 +6,16 @@ module csrrf
    // Control
    read, write, set, clear, imm,
    // Data
-   src_dst, d_rs1, uimm, data_out
+   src_dst, d_rs1, uimm, data_out,
+   // Exceptions
+   exception_machine_trap, exception_permission
    );
    input wire read, write, set, clear, imm;
    input wire [11:0] src_dst;
    input wire [31:0] d_rs1;
    input wire [4:0]  uimm;
    output reg [31:0] data_out;
+
+   
+   
 endmodule // csrrf
