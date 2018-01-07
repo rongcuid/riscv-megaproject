7ab313876cbe463aef2d462811727acd3c563daa
Rongcui Dong <rongcuid@outlook.com>
2018-01-07 03:46:40 -0800

    CSRC tested

--- 01-embedded-softcore-rv32i/core_top_tb.v
+++ 01-embedded-softcore-rv32i/core_top_tb.v
@@ -385,6 +385,28 @@ module core_tb();
 	 end
       end
    endtask //
+
+   // Test 12: CSRC
+   task run_test12;
+      integer 	    i;
+      begin
+	 $display("(TT) --------------------------------------------------");
+	 $display("(TT) Test 12: CSRC Test ");
+	 $display("(TT) 1. On failure, a message is displayed");
+	 $display("(TT) 2. Failure vector is PC=0x10");
+	 $display("(TT) --------------------------------------------------");
+
+	 load_program("tb_out/12-csrc.bin");
+	 hard_reset();
+	 for (i=0; i<48; i=i+1) begin
+	    // $display("(TT) Opcode=%0s, FD_PC=0x%h, x1=0x%h", 
+	    // 	     FD_disasm_opcode, UUT.FD_PC, UUT.RF.data[1]);
+	    if (UUT.FD_PC == 32'h10 || FD_disasm_opcode == "ILLEGAL ")
+	      $display("(TT) Test failed!");
+	    @(posedge clk_tb);
+	 end
+      end
+   endtask //
    
    assign im_data_tb = instruction_memory_tb[im_addr_out_tb[11:2]];
    assign io_data_read_tb = io_memory_tb[io_addr_tb[7:2]];
@@ -430,7 +452,8 @@ module core_tb();
 	// run_test8();
 	// run_test9();
 	// run_test10();
-	run_test11();
+	// run_test11();
+	run_test12();
 
 	$finish;
 	
